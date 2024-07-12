9cf125adf214d2bcbf18e3519f4a87771480e8c1
Silvio Calixto <silvio.calixto.sc@gmail.com>
2024-07-12 15:13:43 -0300

    atualizei

---
+++ ex01/src/Verificador.java
@@ -0,0 +1,14 @@
+public class VerificadorParIdade {
+  public static void main(String[] args) {
+    int numero = 7;
+    String resultado;
+    if (numero % 2 == 0) {
+      resultado = "Par";
+    } else {
+      resultado = "Ímpar";
+    }
+    System.out.println("O numero é: " + resultado); // Sem operador tenário
+  }
+}
+
+public class Verificador
