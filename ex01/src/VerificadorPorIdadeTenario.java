7ba9b194c3fc39efa078b67f5f5c2974a8da5604
Silvio Calixto <silvio.calixto.sc@gmail.com>
2024-07-13 01:08:54 -0300

    criei um novo exemplo

---
+++ ex01/src/VerificadorPorIdadeTenario.java
@@ -0,0 +1,8 @@
+public class VerificadorPorIdadeTenario {
+  public static void main(String[] args) {
+    int numero = 7;
+    String resultado = (numero % 2 == 0) ? "Par" : "Ímpar";
+    System.out.println("O número é: " + resultado);
+  }
+  
+}
