79ec69466291558f147432d1d496ddc96926658c
Silvio Calixto <silvio.calixto.sc@gmail.com>
2024-07-18 02:04:54 -0300

    atualizei

--- exercicios/src/main/java/silviocalixto/AreaTriangulo.java
+++ exercicios/src/main/java/silviocalixto/AreaTriangulo.java
@@ -5,7 +5,17 @@ public class AreaTriangulo {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
+    System.out.println("Digite a base do Triângulo: ");
+    int base = scanner.nextInt();
+
+    System.out.println("Digite a altura do Triângulo: ");
+    int altura = scanner.nextInt();
+
+    int area = (base * altura) / 2;
     
+    System.out.println("A área do Trinângulo é: " + area + " !");
+    
+    scanner.close();
   }
   
 }
